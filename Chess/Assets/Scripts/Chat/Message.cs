7db591d9c7cfe9d8ee85ff333bf327d1716cb3de
Mohamed Elsherif <mohamedelsherif.mo123@gmail.com>
2022-06-27 21:59:35 +0400

    Chat Options

--- Chess/Assets/Scripts/Chat/Message.cs
+++ Chess/Assets/Scripts/Chat/Message.cs
@@ -5,4 +5,75 @@ public class Message : MonoBehaviour
 {
 	public TMP_Text messageText;
 	public TMP_Text usernameText;
+
+	[HideInInspector] public string messageId;
+	[HideInInspector] public string userId;
+
+	public GameObject deleteButton;
+
+	public GameObject updateButton;
+	public TMP_InputField updateInputField;
+	bool updateFieldOpen;
+	
+	public GameObject likeButton;
+	public GameObject reactionObject;
+	public TMP_Text numberOfLikes;
+	bool buttonLikes = true;
+
+	private void Start()
+	{
+		if (StreamChatBehaviour.instance.client.UserId != userId)
+		{
+			deleteButton.SetActive(false);
+			updateButton.SetActive(false);
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Return))
+		{
+			StreamChatBehaviour.instance.UpdateMessage(messageId, updateInputField.text);
+			updateInputField.gameObject.SetActive(false);
+			deleteButton.SetActive(true);
+			updateFieldOpen = false;
+		}
+	}
+
+	public void OnClickDeleteButton()
+	{
+		StreamChatBehaviour.instance.DeleteMessage(messageId);
+		deleteButton.SetActive(false);
+		updateButton.SetActive(false);
+	}
+
+	public void OnClickUpdateButton()
+	{
+		if (!updateInputField.enabled)
+		{
+			updateInputField.gameObject.SetActive(true);
+		}
+
+		if (updateInputField.enabled)
+		{
+			updateInputField.gameObject.SetActive(false);
+		}
+
+		updateInputField.text = messageText.text;
+		updateFieldOpen = true;
+	}
+	
+	public void OnClickLikeButton()
+	{
+		if (buttonLikes)
+		{
+			StreamChatBehaviour.instance.LikeMessage(messageId);
+			buttonLikes = false;
+		}
+		else
+		{
+			StreamChatBehaviour.instance.UnlikeMessage(messageId);
+			buttonLikes = true;
+		}
+	}
 }
--- Chess/Assets/Scripts/Chat/StreamChatBehaviour.cs
+++ Chess/Assets/Scripts/Chat/StreamChatBehaviour.cs
@@ -1,9 +1,8 @@
-using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using StreamChat.Core;
 using StreamChat.Core.Auth;
 using StreamChat.Core.Events;
-using StreamChat.Core.Exceptions;
 using StreamChat.Core.Models;
 using StreamChat.Core.Requests;
 using StreamChat.Libs.Utils;
@@ -11,92 +10,173 @@ using UnityEngine;
 
 public class StreamChatBehaviour : MonoBehaviour
 {
-	public static StreamChatBehaviour instance;
-	
-	IStreamChatClient client;
-	ChannelState channelState;
-	
-	public Message messagePrefab;
-	Transform content;
+    public static StreamChatBehaviour instance;
 
-	private void Awake()
-	{
-		instance = this;
-		DontDestroyOnLoad(this);
-	}
-	
-	public void SetContentObject()
-	{
-		content = GameObject.FindGameObjectWithTag("content").transform;
-	}
-	
-	public void GetOrCreateClient(string userName)
-	{
-		string userId = StreamChatClient.SanitizeUserId(userName);
-		AuthCredentials credentials = new AuthCredentials("xg4h594jgy57", userId, StreamChatClient.CreateDeveloperAuthToken(userId));
-		client = StreamChatClient.CreateDefaultClient(credentials);
-		client.Connect();
-		client.Connected += OnClientConnected;
-		client.MessageReceived += OnMessageRecieved;
-	}
-	
-	void OnClientConnected()
-	{
-		Debug.Log("Stream client connected!");
-	}
-	
-	private void Update()
-	{
-		if (client != null)
-		{
-			client.Update(Time.deltaTime);
-		}
-	}
-	
-	public void GetOrCreateChannel(string roomName)
-	{
-		GetOrCreateChannelAsync(roomName).LogIfFailed();
-	}
-	
-	async Task GetOrCreateChannelAsync(string roomName)
-	{
-		ChannelGetOrCreateRequest request = new ChannelGetOrCreateRequest
-		{
-			State = true,
-			Watch = true
-		};
-		
-		channelState = await client.ChannelApi.GetOrCreateChannelAsync("livestream", roomName, request);
-	}
-	
-	public void SendChat(string message)
-	{
-		SendChatAsync(message, channelState.Channel.Type, channelState.Channel.Id).LogIfFailed();
-	}
-	
-	async Task SendChatAsync(string message, string channelType, string channelId)
-	{
-		SendMessageRequest request = new SendMessageRequest
-		{
-			Message = new MessageRequest
-			{
-				Text = message
-			}
-		};
-		
-		await client.MessageApi.SendNewMessageAsync(channelType, channelId, request);
-	}
-	
-	void OnMessageRecieved(EventMessageNew newMessage)
-	{
-		Message newMessageSpawned = Instantiate(messagePrefab, content);
-		newMessageSpawned.messageText.text = newMessage.Message.Text;
-		newMessageSpawned.usernameText.text = newMessage.User.Id;
-	}
-	
-	public void Disconnect()
-	{
-		client.MessageReceived -= OnMessageRecieved;
-		client.Dispose();
-	}
+    public IStreamChatClient client;
+    ChannelState channelState;
+
+    public Message messagePrefab;
+    Transform content;
+
+    Dictionary<string, Message> idsAndMessages = new Dictionary<string, Message>();
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void SetContentObject()
+    {
+        content = GameObject.FindGameObjectWithTag("content").transform;
+    }
+
+    public void GetOrCreateClient(string userName)
+    {
+        string userId = StreamChatClient.SanitizeUserId(userName);
+        AuthCredentials credentials = new AuthCredentials("xg4h594jgy57", userId, StreamChatClient.CreateDeveloperAuthToken(userId));
+        client = StreamChatClient.CreateDefaultClient(credentials);
+        client.Connect();
+        client.Connected += OnClientConnected;
+        client.MessageReceived += OnMessageReceived;
+
+        client.MessageDeleted += OnMessageDeleted;
+        client.MessageUpdated += OnMessageUpdated;
+        client.ReactionReceived += OnReactionReceived;
+        client.ReactionDeleted += OnReactionDeleted;
+    }
+
+    void OnClientConnected()
+    {
+        Debug.Log("Stream client connected!");
+    }
+
+    private void Update()
+    {
+        if (client != null)
+        {
+            client.Update(Time.deltaTime);
+        }
+    }
+
+    public void GetOrCreateChannel(string roomName)
+    {
+        GetOrCreateChannelAsync(roomName).LogIfFailed();
+    }
+
+    async Task GetOrCreateChannelAsync(string roomName)
+    {
+        ChannelGetOrCreateRequest request = new ChannelGetOrCreateRequest
+        {
+            State = true,
+            Watch = true
+        };
+
+        channelState = await client.ChannelApi.GetOrCreateChannelAsync("livestream", roomName, request);
+    }
+
+    public void SendChat(string message)
+    {
+        SendChatAsync(message, channelState.Channel.Type, channelState.Channel.Id).LogIfFailed();
+    }
+
+    async Task SendChatAsync(string message, string channelType, string channelId)
+    {
+        SendMessageRequest request = new SendMessageRequest
+        {
+            Message = new MessageRequest
+            {
+                Text = message
+            }
+        };
+
+        await client.MessageApi.SendNewMessageAsync(channelType, channelId, request);
+    }
+
+    void OnMessageReceived(EventMessageNew newMessage)
+    {
+        Message newMessageSpawned = Instantiate(messagePrefab, content);
+        newMessageSpawned.messageText.text = newMessage.Message.Text;
+        newMessageSpawned.usernameText.text = newMessage.User.Id;
+        newMessageSpawned.messageId = newMessage.Message.Id;
+        newMessageSpawned.userId = newMessage.User.Id;
+
+        idsAndMessages.Add(newMessage.Message.Id, newMessageSpawned);
+    }
+
+    void OnMessageDeleted(EventMessageDeleted deletedMessage)
+    {
+        Message m = idsAndMessages[deletedMessage.Message.Id];
+        deletedMessage.Message.Text = "Deleted Message";
+        m.messageText.text = deletedMessage.Message.Text;
+        m.likeButton.SetActive(false);
+    }
+
+    void OnMessageUpdated(EventMessageUpdated updatedMessage)
+    {
+        Message m = idsAndMessages[updatedMessage.Message.Id];
+        m.messageText.text = updatedMessage.Message.Text + " (edited)";
+    }
+
+    void OnReactionReceived(EventReactionNew newReaction)
+    {
+        Message m = idsAndMessages[newReaction.Message.Id];
+        m.reactionObject.SetActive(true);
+        m.numberOfLikes.text = newReaction.Message.ReactionScores["like"].ToString();
+    }
+
+    void OnReactionDeleted(EventReactionDeleted deletedReaction)
+    {
+        Message m = idsAndMessages[deletedReaction.Message.Id];
+
+        if (deletedReaction.Message.ReactionScores.ContainsKey("like") == false)
+        {
+            m.reactionObject.SetActive(false);
+        }
+        else
+        {
+            m.numberOfLikes.text = deletedReaction.Message.ReactionScores["like"].ToString();
+        }
+
+    }
+
+    public void DeleteMessage(string messageId)
+    {
+        client.MessageApi.DeleteMessageAsync(messageId, false).LogIfFailed();
+    }
+
+    public void UpdateMessage(string messageId, string updatedMessage)
+    {
+        client.MessageApi.UpdateMessageAsync(new UpdateMessageRequest
+        {
+            Message = new MessageRequest
+            {
+                Id = messageId,
+                Text = updatedMessage
+            }
+        }).LogIfFailed();
+    }
+
+    public void LikeMessage(string messageId)
+    {
+        client.MessageApi.SendReactionAsync(messageId,
+        new SendReactionRequest
+        {
+            Reaction = new ReactionRequest
+            {
+                Type = "like"
+            }
+        }).LogIfFailed();
+    }
+
+    public void UnlikeMessage(string messageId)
+    {
+        client.MessageApi.DeleteReactionAsync(messageId, "like").LogIfFailed();
+    }
+
+    public void Disconnect()
+    {
+        client.MessageReceived -= OnMessageReceived;
+        client.Dispose();
+    }
 }
